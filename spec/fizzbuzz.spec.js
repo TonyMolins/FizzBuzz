a2ea4f0fef80338a9a393cffa0d60e0be40c9d33
TonyMolins <molchinos08@gmail.com>
2015-10-07 10:25:42 -0600

    Fizzbuzz Stuff

---
+++ spec/fizzbuzz.spec.js
@@ -0,0 +1,15 @@
+/*globals require, describe, it, expect*/
+
+
+var fizzbuzz = require ("../fizzbuzz.js");
+ 
+(function () {
+    'use strict';
+
+describe('fizz buzz', function (){
+    it('should return 0 given 0', function () {
+        var result = fizzbuzz.getResult(0);
+        expect(result.toEqual(0));
+    });
+});
+}());
