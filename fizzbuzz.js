6e750975bb5756489dcd35ecc9b47389161426df
TonyMolins <molchinos08@gmail.com>
2015-10-08 09:28:31 -0600

    More Stuff
    Further Content

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,9 @@
-/*globals */
+/*globals exports*/
+
+var fizzbuzz = {
+    getResult: function(number) {
+        return number;
+    }
+};
+
+exports.fizzbuzz = fizzbuzz;
--- spec/fizzbuzz.spec.js
+++ spec/fizzbuzz.spec.js
@@ -1,15 +1,23 @@
 /*globals require, describe, it, expect*/
 
-
-var fizzbuzz = require ("../fizzbuzz.js");
+var fizzbuzz = require("../fizzbuzz.js").fizzbuzz;
  
-(function () {
-    'use strict';
-
 describe('fizz buzz', function (){
+    
     it('should return 0 given 0', function () {
-        var result = fizzbuzz.getResult(0);
-        expect(result.toEqual(0));
+        expect(fizzbuzz.getResult(0)).toEqual(0);
     });
+    
+    it('should return 1 given 1', function () {
+        expect(fizzbuzz.getResult(1)).toEqual(0);
+    });
+    
+    it('should return 2 given 2', function () {
+        expect(fizzbuzz.getResult(2)).toEqual(0);
+    });
+        
+    it('should return fizz given 3', function () {
+        expect(fizzbuzz.getResult(3)).toEqual(0);
+    });
+
 });
-}());
