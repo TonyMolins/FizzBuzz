030e5346c4ff11880969a38427d9be0ebb6271ad
MATC <MATC>
2015-10-13 10:29:44 -0600

    FizzBuzz
    Added stuff

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,18 @@
 /*globals exports*/
 
 var fizzbuzz = {
-    getResult: function(number) {
-        if (number === 3) {
-            return 'Fizz';
-        }
-                if (number === 5) {
-            return 'Buzz';
-        }
-        return number;
+  getResult: function (number) {
+   if (number === 0) {
+      return 0;
     }
+    if (number % 3 === 0) {
+      return 'Fizz';
+    }
+    if (number % 5 === 0) {
+      return 'Buzz';
+    }
+    return number;
+  }
 };
 
 exports.fizzbuzz = fizzbuzz;
