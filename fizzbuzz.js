ff32f8927f847c4aaefb8986b7a9d43eabaf3287
MATC <MATC>
2015-10-14 08:28:09 -0600

    Further Changes

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,21 @@
 /*globals exports*/
 
 var fizzbuzz = {
-  getResult: function (number) {
-   if (number === 0) {
-      return 0;
+    getResult: function (number) {
+        if (number === 0) {
+            return 0;
+        }
+        if (number % 15 === 0) {
+            return 'FizzBuzz';
+        }
+        if (number % 3 === 0) {
+            return 'Fizz';
+        }
+        if (number % 5 === 0) {
+            return 'Buzz';
+        }
+        return number;
     }
-    if (number % 3 === 0) {
-      return 'Fizz';
-    }
-    if (number % 5 === 0) {
-      return 'Buzz';
-    }
-    return number;
-  }
 };
 
 exports.fizzbuzz = fizzbuzz;
--- spec/fizzbuzz.spec.js
+++ spec/fizzbuzz.spec.js
@@ -4,35 +4,45 @@ var fizzbuzz = require("../fizzbuzz.js").fizzbuzz;
 
 describe('fizz buzz', function () {
 
-  it('should return 0 given 0', function () {
-    expect(fizzbuzz.getResult(0)).toEqual(0);
-  });
+    it('should return 0 given 0', function () {
+        expect(fizzbuzz.getResult(0)).toEqual(0);
+    });
 
-  it('should return 1 given 1', function () {
-    expect(fizzbuzz.getResult(1)).toEqual(1);
-  });
+    it('should return 1 given 1', function () {
+        expect(fizzbuzz.getResult(1)).toEqual(1);
+    });
 
-  it('should return 2 given 2', function () {
-    expect(fizzbuzz.getResult(2)).toEqual(2);
-  });
+    it('should return 2 given 2', function () {
+        expect(fizzbuzz.getResult(2)).toEqual(2);
+    });
 
-  it('should return fizz given 3', function () {
-    expect(fizzbuzz.getResult(3)).toEqual('Fizz');
-  });
+    it('should return fizz given 3', function () {
+        expect(fizzbuzz.getResult(3)).toEqual('Fizz');
+    });
 
-  it('should return buzz given 5', function () {
-    expect(fizzbuzz.getResult(5)).toEqual('Buzz');
-  });
+    it('should return buzz given 5', function () {
+        expect(fizzbuzz.getResult(5)).toEqual('Buzz');
+    });
 
-  it('should return fizz given 6', function () {
-    expect(fizzbuzz.getResult(6)).toEqual('Fizz');
-  });
-  it('should return buzz given 10', function () {
-    expect(fizzbuzz.getResult(10)).toEqual('Buzz');
-  });
-  it('should return fizzbuzz given 15', function () {
-    expect(fizzbuzz.getResult(15)).toEqual('FizzBuzz');
-  });
+    it('should return fizz given 6', function () {
+        expect(fizzbuzz.getResult(6)).toEqual('Fizz');
+    });
+
+    it('should return fizz given 9', function () {
+        expect(fizzbuzz.getResult(9)).toEqual('Fizz');
+    });
+
+    it('should return buzz given 10', function () {
+        expect(fizzbuzz.getResult(10)).toEqual('Buzz');
+    });
+
+    it('should return fizz given 12', function () {
+        expect(fizzbuzz.getResult(12)).toEqual('Fizz');
+    });
+
+    it('should return fizzbuzz given 15', function () {
+        expect(fizzbuzz.getResult(15)).toEqual('FizzBuzz');
+    });
 
 
 });
